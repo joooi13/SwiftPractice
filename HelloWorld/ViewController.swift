eb1eb39e6cab72379da9a4237456480e9f6f03d1
yu joi <yujoi@joiy.local>
2016-10-31 23:28:22 +0900

    Chapter4_02

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -10,13 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var priceField: UITextField!
     @IBOutlet weak var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //引き算の結果をvalueに
-        let value :Int = 10 - 1
-        label.text = "10 - 1 = \(value)"
+        
     }
 
     override func didReceiveMemoryWarning() {
